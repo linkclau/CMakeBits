918e45d0c9ad63dc9761dd9402dacdab6b44be3b
Claudius Link <claudius.link@gmail.com>
2016-06-24 11:40:45 +0200

    Build an use the library

---
+++ Tutorial/MathFunctions/mysqrt.cxx
@@ -0,0 +1,11 @@
+#include "mysqrt.h"
+
+#include <cmath>
+
+#include <iostream>
+
+double mysqrt(double x)
+{
+  std::cout << "Heyho\n";
+  return sqrt( x);
+}
--- Tutorial/tutorial.cxx
+++ Tutorial/tutorial.cxx
@@ -1,8 +1,9 @@
 // A simple program that computes the square root of a number
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
 #include "TutorialConfig.h"
+#include <mysqrt.h>
  
 int main (int argc, char *argv[])
 {
@@ -16,7 +17,7 @@ int main (int argc, char *argv[])
     return 1;
     }
   double inputValue = atof(argv[1]);
-  double outputValue = sqrt(inputValue);
+  double outputValue = mysqrt(inputValue);
   fprintf(stdout,"The square root of %g is %g\n",
           inputValue, outputValue);
   return 0;
