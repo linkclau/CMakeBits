4149ff581d6ea4d0ed081d2feb5364f78122f6b3
Claudius Link <claudius.link@gmail.com>
2016-08-01 15:04:25 +0200

    Compiling and not crashing example on how to use libzip It doesn't creat an zip file so.

--- LibZiPTool/libziptool.cpp
+++ LibZiPTool/libziptool.cpp
@@ -5,6 +5,7 @@
 #include <zip.h>
 
 #include <iostream>
+#include <memory>
 
 int main( int argc, char** argv ) {
   if( argc < 3 ) {
@@ -21,19 +22,24 @@ int main( int argc, char** argv ) {
     return 1;
   }
 
-  struct zip_source *s;
-  char buffer[1024*1024*10]="";
-  s=zip_source_buffer(archive, buffer, sizeof(buffer), 0);
+  struct zip_source *s = nullptr;
+  // 10 MB are too big for the stack
+  // char buffer[1024*1024*10]="";
+
+  std::unique_ptr<char[]> buffer(new char[1024*1024*10] );
+  s=zip_source_buffer(archive, buffer.get(), 1024*1024*10, 0);
   if (s == nullptr ) {
     //printf("error open source: %s", zip_strerror(archive));
     return 1;
   }
 
-  for( int i = 2; i<argc; ++i) {
+  std::cout << "argc " << argc  << std::endl;
+
+  for( int i = 2; i<argc; i++) {
     std::cout << i << ": " << argv[i] << std::endl;
     zip_add( archive, argv[i], s);
   }
-  zip_source_free(s);
+  //zip_source_free(s);
   
   if( zip_close( archive ) != 0 ) {
     // chekc the error code in the archive
